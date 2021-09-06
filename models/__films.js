75f8e3c09cef07922a7bf6aa9d8050479323eebe
Vlad Imir <admin@MacBook-Pro-admin.local>
2021-09-06 22:45:26 +0300

    new branch

--- models/__films.js
+++ models/__films.js
@@ -1,7 +1,27 @@
 
 const sequelize = require('../database')
+const { Sequelize, DataTypes } = require('sequelize');
 
 class Films{
+
+    const sequelize = new Sequelize('sqlite::memory:');
+
+    const User = sequelize.define('User', {
+        // Model attributes are defined here
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING
+            // allowNull defaults to true
+        }
+    }, {
+        // Other model options go here
+    });
+
+    // `sequelize.define` also returns the model
+    //console.log(User === sequelize.models.User); // true
 
     // static async addSimilarFilm(film_id, similar_film_id, from = 'kp'){
     //     const [results] =  await sequelize.query("INSERT IGNORE INTO similars (`film_id`, `similar_film_id`, `from`)"
@@ -9,6 +29,8 @@ class Films{
     //
     //     return results
     // }
+
+    /*
 
     static async getIdByUrl(url){
         const [results] =  await sequelize.query(`SELECT id, status FROM films WHERE url_kp='${url}'`);
@@ -73,7 +95,7 @@ class Films{
         const [results] =  await sequelize.query(`SELECT * FROM films WHERE status = ${status}  ORDER BY RAND() LIMIT 0,1`);
         return  results
     }
-
+  */
 }
 
 module.exports = Films
